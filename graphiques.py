acfef2a7c18ff7eb6b64e1ae959ce266f1b6be19
lcollin33v <leo.collinh@gmail.com>
2022-05-05 13:12:25 +0200

    fonctions pour colorier les cases, et cbateau

--- graphiques.py
+++ graphiques.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+bleu = '#159CDA'
+blanc = '#DDF4FF'
+rouge = '#FF5454'
+
 
 cases = [[[(k, i), (k + 50, i), (k, i + 50), (k + 50, i + 50)] for k in range(0, 500, 50)] for i in range(0, 500, 50)]
 
@@ -17,6 +21,10 @@ class Bateau:
             aire_jeu1.create_rectangle(cases[x][y][i] for i in range(4))
         else:
             aire_jeu2.create_rectangle(cases[x][y][i] for i in range(4))
+    
+    def placer_bateau(self):
+        pass
+
 
 #verification qu'une case choisie est valide 
 def Valide(bateau, case_choisie):
@@ -25,7 +33,7 @@ def Valide(bateau, case_choisie):
     for case in cases_a_cote:
         if [case_choisie[0] + case[0], case_choisie[1] + case[1]] in bateau.liste_cases:
             valide = True
-        if  [case_choisie[0] + case[0], case_choisie[1] + case[1]] in appartenance_bateau:
+        if  (case_choisie[0] + case[0], case_choisie[1] + case[1]) in appartenance_bateau:
             valide = False
 
     if valide:
@@ -40,22 +48,44 @@ def Croix(x1, y1, x2, y2, aire_jeu):
     aire_jeu.create_line(x1,y2,x2,y1)
 
 
-def Grille(): #création de la grille de base
+def Grille(aire_jeu): #création de la grille de base
     for colonne in range(1, 10):
-        aire_jeu1.create_line(50 * colonne, 0, 50 * colonne, 500, fill = 'black')
-        aire_jeu1.create_line(0, 50 * colonne, 500, 50 * colonne, fill = 'black')
+        aire_jeu.create_line(25 * colonne, 0, 25 * colonne, 250, fill = blanc)
+        aire_jeu.create_line(0, 25 * colonne, 250, 25 * colonne, fill = blanc)
 
-        aire_jeu2.create_line(50 * colonne, 0, 50 * colonne, 500, fill = 'black')
-        aire_jeu2.create_line(0, 50 * colonne, 500, 50 * colonne, fill = 'black')
+def Case(x1,y1,x2,y2, aire_jeu):
+    aire_jeu.create_rectangle(x1,y1,x2,y2,fill = rouge,outline= blanc)
 
+def cbateau(event):
+    clic=(event.x,event.y)
+    coord=0
+    a=0
+    b=0
+    while coord==0:
+        if clic[1]>cases[a][b][-1][-1]:
+            a+=1
+        if clic[0]>cases[a][b][-1][0]:
+            b+=1
+        else:
+            if clic[1]<cases[a][b][-1][-1]:
+                coord=[cases[a][b][0],cases[a][b][-1]]
+    x1,y1,x2,y2=coord[0][0],coord[0][1],coord[1][0],coord[1][1]
+    Case(x1,y1,x2,y2,aire_jeu1)
+    
 fen = Tk()
 
-aire_jeu1 = Canvas(fen, width = 500, height = 500, bg = 'lightgrey')
+aire_jeu1 = Canvas(fen, width = 250, height = 250, bg = bleu)
 aire_jeu1.pack(side = LEFT, padx = 10, pady = 10)
-aire_jeu2 = Canvas(fen, width = 500, height = 500, bg = 'lightgrey')
+aire_jeu2 = Canvas(fen, width = 250, height = 250, bg = bleu)
 aire_jeu2.pack(side = RIGHT, padx = 10, pady = 10)
-b = Button(text = 'b', command=lambda : Croix(0,0,50, 50, aire_jeu= aire_jeu1))
+b = Button(text = 'b', command=lambda : Croix(0,0,25, 25, aire_jeu= aire_jeu1))
 b.pack()
-Grille()
+Grille(aire_jeu1)
+Grille(aire_jeu2)
+aire_jeu1.bind("<Button-1>",cbateau)
+c = Button(text = 'c', command=lambda : Case(25,0,50, 25, aire_jeu= aire_jeu1))
+c.pack()
+
+
 
 fen.mainloop()
