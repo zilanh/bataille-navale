5ebba1c4a1202c7542750a5afd36030437435422
lcollin33v <leo.collinh@gmail.com>
2022-05-06 14:39:08 +0200

    séparation identification de case

--- graphiques.py
+++ graphiques.py
@@ -7,8 +7,8 @@ rouge = '#FF5454'
 
 cases = [[[(k, i), (k + 25, i), (k, i + 25), (k + 25, i + 25)] for k in range(0, 250, 25)] for i in range(0, 250, 25)]
 
-appartenance_bateau_j1 = [[False for i in range(8)] for k in range(8)]
-appartenance_bateau_j2 = [[False for i in range(8)] for k in range(8)]
+appartenance_bateau_j1 = [[False for i in range(10)] for k in range(10)]
+appartenance_bateau_j2 = [[False for i in range(10)] for k in range(10)]
 
 class Bateau:
     def __init__(self, longueur, liste_cases, joueur):
@@ -38,24 +38,10 @@ def Valide(bateau, case_choisie, appartenance_bateau):
 
 #action de tirer sur une case, comprend le cas où un bateau est touché et quand aucun n'est touché
 def Tir(event, appartenance_bateau): 
-    #if (x, y) in appartenance_bateau:
-    #    appartenance_bateau[(x, y)].touche(x, y)
-    clic = (event.x,event.y)
-    coord = 0
-    a = 0
-    b = 0
-    while coord == 0:
-        if clic[1] > cases[a][b][-1][-1]:
-            a += 1
-        if clic[0] > cases[a][b][-1][0]:
-            b += 1
-        else:
-            if clic[1] <= cases[a][b][-1][-1]:
-                coord = [cases[a][b][0], cases[a][b][-1]]
 
-    x1, y1, x2, y2 = coord[0][0], coord[0][1], coord[1][0], coord[1][1]
+    a, b, x1, y1, x2, y2 = IdentificationCase(event)
 
-    if (a, b) in appartenance_bateau:
+    if appartenance_bateau[a][b]:
         Case(x1, y1, x2, y2, aire_jeu1)
     else:
         Croix(x1, y1, x2, y2, aire_jeu1)
@@ -73,15 +59,36 @@ def Grille(aire_jeu): #création de la grille de base
 def Case(x1, y1, x2, y2, aire_jeu):
     aire_jeu.create_rectangle(x1, y1, x2, y2, fill = rouge,outline= blanc)
 
-    
+def IdentificationCase(event):
+    clic = (event.x,event.y)
+    coord = 0
+    a = 0
+    b = 0
+    while coord == 0:
+        if clic[1] > cases[a][b][-1][-1]:
+            a += 1
+        if clic[0] > cases[a][b][-1][0]:
+            b += 1
+        else:
+            if clic[1] <= cases[a][b][-1][-1]:
+                coord = [cases[a][b][0], cases[a][b][-1]]
+
+    x1, y1, x2, y2 = coord[0][0], coord[0][1], coord[1][0], coord[1][1]
+
+    return a, b, x1, y1, x2, y2
+
+appartenance_bateau_j1[2][3] = True
+appartenance_bateau_j1[9][9] = True
+
+
 fen = Tk()
 
-aire_jeu1 = Canvas(fen, width = 250, height = 250, bg = bleu)
+aire_jeu1 = Canvas(fen, width = 249, height = 249, bg = bleu)
 aire_jeu1.pack(side = LEFT, padx = 10, pady = 10)
-aire_jeu2 = Canvas(fen, width = 250, height = 250, bg = bleu)
+aire_jeu2 = Canvas(fen, width = 249, height = 249, bg = bleu)
 aire_jeu2.pack(side = RIGHT, padx = 10, pady = 10)
 Grille(aire_jeu1)
 Grille(aire_jeu2)
-aire_jeu1.bind("<Button-1>", Tir)
+aire_jeu1.bind("<Button-1>", lambda event : Tir(event, appartenance_bateau_j1))
 
 fen.mainloop()
